56c657338f73eec322d55ab4229a0672f98865ef
Nick Beaugeard <nick@hubone.com>
2018-09-16 09:11:01 +1000

    First Check in

---
+++ CSOMCoreTests/CSOMCoreTests/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.SharePoint.Client;
+
+namespace CSOMCoreTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
